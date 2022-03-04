02d6ee7a0a06963d1f76206eb5c57f19aba8ba22
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-04 16:00:44 -0600

    index and show

--- app/controllers/bbqs_controller.rb
+++ app/controllers/bbqs_controller.rb
@@ -1,2 +1,12 @@
 class BbqsController < ApplicationController
+
+    def index
+        bbqs = Bbq.all
+        render json: bbqs
+    end
+
+    def show
+        bbq = Bbq.find(params[:id])
+        render json: bbq
+    end
 end
