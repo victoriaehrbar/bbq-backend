04856d0a551f09f8e0326de7b96d32495126f517
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-04 21:12:28 -0600

    bbq params

--- app/controllers/bbqs_controller.rb
+++ app/controllers/bbqs_controller.rb
@@ -9,4 +9,17 @@ class BbqsController < ApplicationController
         bbq = Bbq.find(params[:id])
         render json: bbq
     end
+
+    def create
+        bbq = Bbq.new(bbq_params)
+        if bbq.save
+            render json: bbq
+        end
+    end
+
+    private
+
+    def bbq_params
+        params.require(:bbq).permit(:name, :rating, :review)
+    end
 end
